b11589102da2cc588046c971361b2091bd9c1335
Raikiri45 <149266085+Raikiri45@users.noreply.github.com>
2024-05-30 02:58:52 +0100

    Check for fetch and convert data to dict

--- RandomQuoteGenerator.py
+++ RandomQuoteGenerator.py
@@ -3,5 +3,9 @@ import requests
 # Fetches quotes from api
 response = requests.get("https://type.fit/api/quotes")
 
-
+# Checks if the fetch went smoothly
+if response.status_code == 200:
+    # Converts data to dictionary
+    quotes =  response.json()
+    
     
