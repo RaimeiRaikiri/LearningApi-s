0d9d23c7fd1c334dd1c39ceb7018cd5d4982d73f
Raikiri45 <149266085+Raikiri45@users.noreply.github.com>
2024-05-30 04:01:48 +0100

    Print random quote from the dict

--- RandomQuoteGenerator.py
+++ RandomQuoteGenerator.py
@@ -1,4 +1,4 @@
-import requests
+import requests, random
 
 # Fetches quotes from api
 response = requests.get("https://type.fit/api/quotes")
@@ -7,5 +7,8 @@ response = requests.get("https://type.fit/api/quotes")
 if response.status_code == 200:
     # Converts data to dictionary
     quotes =  response.json()
+    # Printing a random quote from the dict
+    print("Quote: " + quotes[random.randrange(0,16)]['text'])
     
+
     
